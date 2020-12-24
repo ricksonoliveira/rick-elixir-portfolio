4c87b2552a6e13f613d36c6d922cf0cce2e202fc
Rick G <riksongleison471@gmail.com>
2020-12-24 15:21:04 -0300

    CardLive Route

---
+++ lib/trello_tasker_web/live/card_infod_live.ex
@@ -0,0 +1,17 @@
+defmodule TrelloTaskerWeb.CardInfoLive do
+  use TrelloTaskerWeb, :live_view
+
+  alias Phoenix.View
+  alias TrelloTaskerWeb.CardView
+
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def render(assings) do
+    View.render(CardView, "info.html", assings)
+  end
+end
